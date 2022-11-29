2f6cd1c25ea0ceb637c55fe6e95630c36e564cf6
LilleAila <olai.solsvik@gmail.com>
2022-11-29 15:23:28 +0100

    try to fix sudo issues

--- install.sh
+++ install.sh
@@ -5,12 +5,12 @@ dnf -y install Xorg xinit i3-gaps
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
 # Install RPM Fusion
-dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y update
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y update
 
 # Install packages
-dnf -y install git zsh vim arandr lxappearance alacritty polybar j4-dmenu-desktop light picom playerctl feh bat httpie exa the_silver_searcher papirus-icon-theme flameshot ffmpeg-libs util-linux-user
+sudo dnf -y install git zsh vim arandr lxappearance alacritty polybar j4-dmenu-desktop light picom playerctl feh bat httpie exa the_silver_searcher papirus-icon-theme flameshot ffmpeg-libs util-linux-user
 
 # Install fonts
 http -d https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FiraCode.zip
@@ -30,11 +30,11 @@ fc-cache -fv
 
 # Dotfiles
 # git clone https://github.com/LilleAila/i3-config.git
-mkdir -p $HOME/Pictures
-mkdir -p $HOME/.config
+mkdir -p ~/Pictures
+mkdir -p ~/.config
 # cd i3-config
-cp background.jpg $HOME/Pictures/
-cp config/* $HOME/.config/*
+cp background.jpg ~/Pictures/
+cp config/* ~/.config/*
 
 # Betterlockscreen
 http -d https://github.com/Raymo111/i3lock-color/releases/latest/download/i3lock
@@ -45,8 +45,8 @@ http -d -o betterlockscreen https://raw.githubusercontent.com/betterlockscreen/b
 chmod +x betterlockscreen
 mv betterlockscreen /usr/local/bin
 
-dnf -y copr enable aflyhorse/libjpeg
-dnf -y install libjpeg8 xrdb xset xdpyinfo
+sudo dnf -y copr enable aflyhorse/libjpeg
+sudo dnf -y install libjpeg8 xrdb xset xdpyinfo
 betterlockscreen -u ~/Pictures/background.jpg
 
 # Info
